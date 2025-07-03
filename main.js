92a17d7e7d4b91eee15b189767f266dc3a129c42
John Hoover <FiveMonths2Full@outlook.com>
2025-07-03 11:41:01 -0400

    fix: correct script tag attribute and add basic arithmetic functions

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function add (a,b) {
     return a + b;
 }
+function subtract (a,b) {
+    return a - b;
+}
+function multiply (a,b) {
+    return a * b;
+}
+function divide (a,b) {
+    b !== 0 ? a/b : undefined;
+}
+console.log(add(4,0));
+console.log(subtract(4,0));
+console.log(multiply(4,0));
+console.log(divide(4,0));
